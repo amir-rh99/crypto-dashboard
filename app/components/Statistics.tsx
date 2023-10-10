91528d73f856eedd23c9d4cd2911d86c1a03b441
AmiReza Hammamian <amir.astar20@gmail.com>
2023-10-10 21:47:13 +0330

    create stattistics component

---
+++ app/components/Statistics.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Statistics = () => {
+  return (
+    <div>
+        <h6 className="text-title font-medium text-lg mb-5">Statistics</h6>
+        <div>
+            <img src="/images/chart2.png" className="w-full"/>
+        </div>
+    </div>
+  )
+}
+
+export default Statistics
