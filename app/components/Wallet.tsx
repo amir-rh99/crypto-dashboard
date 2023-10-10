39c9b976287141f43cf12578386ff93bed432058
AmiReza Hammamian <amir.astar20@gmail.com>
2023-10-10 21:14:33 +0330

    create wallet and wallet item cmp

---
+++ app/components/Wallet.tsx
@@ -0,0 +1,11 @@
+import WalletItem from "./WalletItem"
+
+const Wallet = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <WalletItem />
+    </div>
+  )
+}
+
+export default Wallet
