eede6ca72f0332584a8d78f71d9cd0ef5d3aca07
AmiReza Hammamian <amir.astar20@gmail.com>
2023-10-10 22:39:18 +0330

    install @headlessui/react for tailwind components

--- app/components/Transaction.tsx
+++ app/components/Transaction.tsx
@@ -1,23 +1,88 @@
+"use client"
+
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
+
 const Transaction = () => {
   return (
     <div>
         <div className="mb-5">
-        <span>Buy</span>
+          <span>Buy</span>
         </div>
-        <div className="flex justify-center items-center gap-5 font-semibold">
-        <span>
-            <span className="text-title mx-1">1</span>
-            <span className="text-info text-xs">BTC</span>
-        </span>
-        <span>
-            <svg  className="w-5 h-5 stroke-title" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-            </svg>
-        </span>
-        <span>
-            <span className="text-title mx-1">42,565.20</span>
-            <span className="text-info text-xs">USD</span>
-        </span>
+        <div className="flex justify-center items-center gap-5 font-semibold mb-10">
+          <span>
+              <span className="text-title mx-1">1</span>
+              <span className="text-info text-xs">BTC</span>
+          </span>
+          <span>
+              <svg  className="w-5 h-5 stroke-title" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+              </svg>
+          </span>
+          <span>
+              <span className="text-title mx-1">42,565.20</span>
+              <span className="text-info text-xs">USD</span>
+          </span>
+        </div>
+        <div>
+        <Menu as="div" className="relative">
+        <div>
+          <Menu.Button 
+          className="bg-body border border-info/40 rounded-full p-3 flex items-center divide-x-2 divide-info/40 w-full transition-all hover:border-info">
+            <div className='flex items-center gap-2 pr-2'>
+              <span>
+                <svg className='w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g fill="none"><circle cx="16" cy="16" r="16" fill="#F3BA2F"/><path fill="#FFF" d="M12.116 14.404L16 10.52l3.886 3.886 2.26-2.26L16 6l-6.144 6.144 2.26 2.26zM6 16l2.26-2.26L10.52 16l-2.26 2.26L6 16zm6.116 1.596L16 21.48l3.886-3.886 2.26 2.259L16 26l-6.144-6.144-.003-.003 2.263-2.257zM21.48 16l2.26-2.26L26 16l-2.26 2.26L21.48 16zm-3.188-.002h.002V16L16 18.294l-2.291-2.29-.004-.004.004-.003.401-.402.195-.195L16 13.706l2.293 2.293z"/></g></svg>
+              </span>
+              <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}       stroke="currentColor" className="w-4 stroke-info">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+              </svg>
+              </span>
+            </div>
+            <div className='pl-2 font-medium'>
+              <span className='text-title text-sm'>Bitcoin</span>
+              <span className='text-info px-2 text-xs'>BTC</span>
+            </div>
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1 ">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+
+                      Edit
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Duplicate
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
         </div>
     </div>
   )
