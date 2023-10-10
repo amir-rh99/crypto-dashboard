3d0fa5f1f2454d360a0487452f2244107fbfb85e
AmiReza Hammamian <amir.astar20@gmail.com>
2023-10-10 21:14:53 +0330

    create transaction & item cmp

---
+++ app/components/TransactionList.tsx
@@ -0,0 +1,11 @@
+import TransactionItem from "./TransactionItem"
+
+const TransactionList = () => {
+  return (
+    <div className="flex flex-col gap-2">
+        <TransactionItem />
+    </div>
+  )
+}
+
+export default TransactionList
