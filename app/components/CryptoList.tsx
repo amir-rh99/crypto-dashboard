d7147d2da41c7fa2070cc39340ad0ced8401eb5a
AmiReza Hammamian <amir.astar20@gmail.com>
2023-10-10 21:46:59 +0330

    create crypto and crypto list component

---
+++ app/components/CryptoList.tsx
@@ -0,0 +1,11 @@
+import Crypto from "./Crypto"
+
+const CryptoList = () => {
+  return (
+    <div className="grid grid-cols-5 gap-5">
+        <Crypto />
+    </div>
+  )
+}
+
+export default CryptoList
