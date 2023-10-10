93975f17e6d245a33002d6c8cc37964bab4c69aa
AmirReza Hammamian <amir.astar20@gmail.com>
2023-10-10 15:46:04 +0330

    add main colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,16 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'title': '#fff',
+        'info': '#4E515E',
+        'card': '#1A1B22',
+        'body': "#131517",
+        'primary': "#FF9F10",
+      }
     },
   },
   plugins: [],
