4e0e302b2d889e2ef819914424f68712897e4f90
AmirReza Hammamian <amir.astar20@gmail.com>
2023-10-11 11:14:48 +0330

    add Tabs to Statistics section

--- app/components/Statistics.tsx
+++ app/components/Statistics.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Tabs from './Tabs'
 
 const Statistics = () => {
   return (
     <div>
-        <h6 className="text-title font-medium text-lg mb-5">Statistics</h6>
+        <div className='flex justify-between items-center mb-5'>
+          <h6 className="text-title font-medium text-lg">Statistics</h6>
+          <div>
+            <Tabs tabs={['1h', '1d', '1w', '2w', '1m', '1y']} />
+          </div>
+        </div>
         <div>
             <img src="/images/chart2.png" className="w-full"/>
         </div>
